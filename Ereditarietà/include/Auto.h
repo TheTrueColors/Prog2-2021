6ab0ecebf69323e2535726f3e6570819bac43ab7
TheTrueColors <81364179+TheTrueColors@users.noreply.github.com>
2021-06-11 14:25:23 +0200

    Uploading startg files

---
+++ Ereditarietà/include/Auto.h
@@ -0,0 +1,18 @@
+#ifndef AUTO_H
+#define AUTO_H
+
+#include <Veicolo.h>
+
+
+class Auto : public Veicolo
+{
+    public:
+        Auto() {}
+        ~Auto() {}
+
+    protected:
+
+    private:
+};
+
+#endif // AUTO_H
---
+++ Ereditarietà/include/Veicolo.h
@@ -0,0 +1,16 @@
+#ifndef VEICOLO_H
+#define VEICOLO_H
+
+
+class Veicolo
+{
+    public:
+        Veicolo() {}
+        virtual ~Veicolo() {}
+
+    protected:
+
+    private:
+};
+
+#endif // VEICOLO_H
